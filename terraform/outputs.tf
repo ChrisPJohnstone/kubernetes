7eef82ec013c9ff3db73d4060a43b68c2d4286e8
Christopher Johnstone <chrisj97@proton.me>
2026-06-20 13:38:03 +0100

    Added worker nodes back in

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -2,10 +2,10 @@ output "gaffer_ip" {
   value = module.gaffer.guest_ip
 }
 
-# output "hoddit_ip" {
-#   value = module.hoddit.guest_ip
-# }
-#
-# output "doddit_ip" {
-#   value = module.doddit.guest_ip
-# }
+output "hoddit_ip" {
+  value = module.hoddit.guest_ip
+}
+
+output "doddit_ip" {
+  value = module.doddit.guest_ip
+}
